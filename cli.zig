541bd5d399765e9addecc9726dd4921973f0165f
Jorge <jorgegarcia_tx@outlook.com>
2026-01-18 23:03:00 -0600

    Naming fixes, keeping path of files execept for extensions

--- cli.zig
+++ cli.zig
@@ -10,7 +10,6 @@ const Chameleon = @import("chameleon");
 
 const config = @import("config.zig");
 const compiler = @import("compiler.zig");
-const buildRunner = @import("buildsys/buildRunner.zig");
 
 const MAJOR_VERSION = 0;
 const MINOR_VERSION = 1;
@@ -75,11 +74,6 @@ fn parseArgs() ![][]const u8 {
   var cli = cliargs.rootCommand();
   cli.setProperty(.help_on_empty_args);
 
-  try cli.addSubcommands(&[_]Command{
-    cliargs.createCommand("build", "Build the project"),
-    cliargs.createCommand("init", "Initialize a new Rulang project"),
-  });
-
   try cli.addArgs(&[_]Arg{
     Arg.singleValueOption("output", 'o', "Output file"),
     Arg.booleanOption("version", 'V', "Show version and exit"),
@@ -92,15 +86,6 @@ fn parseArgs() ![][]const u8 {
   try cli.addArg(Arg.multiValuesPositional("files", null, null));
 
   const matches = try cliargs.parseProcess();
-
-  if (matches.subcommandMatches("build")) |buildMatch| {
-    debug(.DBG_BASIC, "Matched subcommand build\n", .{});
-    try buildRunner.runBuild(buildMatch);
-  }
-
-  if (matches.containsArg("build")) {
-    debug(.DBG_BASIC, "Build arg.\n", .{});
-  }
 
   if (matches.containsArg("version")) {
     try stdout.print("Rulang Compiler version {}.{}.{}\n", .{ MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION });
@@ -149,23 +134,23 @@ fn parseArgs() ![][]const u8 {
     cfg.compileOnly = true;
   }
 
-  const raw_files = matches.getMultiValues("files").?;
+  const files = matches.getMultiValues("files").?;
 
   // Duplicate the file names into our allocator so they outlive `cliargs`
-  var files_list = try std.ArrayList([]const u8).initCapacity(allocator, raw_files.len);
-  defer files_list.deinit(allocator);
-  for (raw_files) |f| {
+  var fileList = try std.ArrayList([]const u8).initCapacity(allocator, files.len);
+  defer fileList.deinit(allocator);
+  for (files) |f| {
     const dup = try allocator.dupe(u8, f);
-    try files_list.append(allocator, dup);
+    try fileList.append(allocator, dup);
   }
 
   // Return an owned slice allocated from `allocator` containing the file name slices.
-  return try files_list.toOwnedSlice(allocator);
+  return try fileList.toOwnedSlice(allocator);
 }
 
 fn callAssembler(filename: []const u8) !std.process.Child.Term {
-  var args_list = try std.ArrayList([]const u8).initCapacity(allocator, 8);
-  defer args_list.deinit(allocator);
+  var cmdList = try std.ArrayList([]const u8).initCapacity(allocator, 8);
+  defer cmdList.deinit(allocator);
 
   var allocated = try std.ArrayList([]u8).initCapacity(allocator, 8);
   defer {
@@ -174,32 +159,38 @@ fn callAssembler(filename: []const u8) !std.process.Child.Term {
   }
 
   // program name
-  try args_list.append(allocator, "arxsm");
+  try cmdList.append(allocator, "arxsm");
 
   // -o <filename>.ao
-  try args_list.append(allocator, "-o");
+  try cmdList.append(allocator, "-o");
   const outname = try std.fmt.allocPrint(allocator, "{s}.ao", .{filename});
   try allocated.append(allocator, outname);
-  try args_list.append(allocator, outname);
+  try cmdList.append(allocator, outname);
 
   // <filename>.s
   const asmname = try std.fmt.allocPrint(allocator, "{s}.s", .{filename});
   try allocated.append(allocator, asmname);
-  try args_list.append(allocator, asmname);
+  try cmdList.append(allocator, asmname);
 
   // assembler args: each prefixed with '-'
   for (cfg.assemblerArgs) |a| {
     const pref = try std.fmt.allocPrint(allocator, "-{s}", .{a});
     try allocated.append(allocator, pref);
-    try args_list.append(allocator, pref);
+    try cmdList.append(allocator, pref);
   }
 
   if (cfg.useDebugSymbols) {
-    try args_list.append(allocator, "-g");
+    try cmdList.append(allocator, "-g");
   }
 
   // Execute the process and wait for it to finish
-  const argv = args_list.items;
+  const argv = cmdList.items;
+
+  debug(.DBG_TRACE, "Assembler command:\n", .{});
+  for (argv) |arg| {
+    debug(.DBG_TRACE, " {s}", .{arg});
+  }
+  debug(.DBG_TRACE, "\n", .{});
 
   var proc = std.process.Child.init(argv, allocator);
   proc.spawn() catch |err| {
@@ -217,43 +208,43 @@ fn callAssembler(filename: []const u8) !std.process.Child.Term {
 fn callLinker(files: std.ArrayList([]const u8)) !void {
   debug(.DBG_BASIC, "Linking {d} object files...\n", .{files.items.len});
 
-  var args_list = try std.ArrayList([]const u8).initCapacity(allocator, 8 + files.items.len);
-  defer args_list.deinit(allocator);
+  var cmdList = try std.ArrayList([]const u8).initCapacity(allocator, 8 + files.items.len);
+  defer cmdList.deinit(allocator);
 
   // program name
-  try args_list.append(allocator, "arxlnk");
+  try cmdList.append(allocator, "arxlnk");
 
   // -o <output file>
-  try args_list.append(allocator, "-o");
-  try args_list.append(allocator, cfg.outbin);
+  try cmdList.append(allocator, "-o");
+  try cmdList.append(allocator, cfg.outbin);
 
   // <filename>.ao for each file
   for (files.items) |filename| {
     const objname = try std.fmt.allocPrint(allocator, "{s}.ao", .{filename});
     debug(.DBG_BASIC, "Linking object file: {s}\n", .{objname});
     // defer allocator.free(objname);
-    try args_list.append(allocator, objname);
+    try cmdList.append(allocator, objname);
   }
 
   // linker args: each prefixed with '-'
   for (cfg.linkerArgs) |a| {
     const pref = try std.fmt.allocPrint(allocator, "-{s}", .{a});
     // defer allocator.free(pref);
-    try args_list.append(allocator, pref);
+    try cmdList.append(allocator, pref);
   }
 
   // Execute the process and wait for it to finish
-  const argv = args_list.items;
+  const argv = cmdList.items;
 
-  var cmd_buf: [2048]u8 = undefined;
-  var cmd_pos: usize = 0;
-  for (args_list.items) |arg| {
-    const p = try std.fmt.bufPrint(cmd_buf[cmd_pos..], "{s} ", .{arg});
-    cmd_pos += p.len;
+  var cmdBuf: [2048]u8 = undefined;
+  var cmdPos: usize = 0;
+  for (cmdList.items) |arg| {
+    const p = try std.fmt.bufPrint(cmdBuf[cmdPos..], "{s} ", .{arg});
+    cmdPos += p.len;
   }
-  const p1 = try std.fmt.bufPrint(cmd_buf[cmd_pos..], "\n", .{});
-  cmd_pos += p1.len;
-  debug(.DBG_BASIC, "Running linker command: {s}", .{cmd_buf[0..cmd_pos]});
+  const p1 = try std.fmt.bufPrint(cmdBuf[cmdPos..], "\n", .{});
+  cmdPos += p1.len;
+  debug(.DBG_BASIC, "Running linker command: {s}", .{cmdBuf[0..cmdPos]});
 
   var proc = std.process.Child.init(argv, allocator);
   proc.spawn() catch |err| {
@@ -295,15 +286,14 @@ pub fn main() !void {
 
   var idx: usize = 0;
   for (infiles) |infile| {
-    debug(.DBG_BASIC, "Input file {d}: {s} ", .{idx, infile});
+    debug(.DBG_BASIC, "Input file {d}: '{s}' ", .{idx, infile});
 
     const ext = std.fs.path.extension(infile);
     debug(.DBG_BASIC, "with extension: {s}\n", .{ext});
     if (ext.len != 0) {
       if (std.mem.eql(u8, ext, ".ru")) {
         // Remove .ru extension
-        const bname = std.fs.path.basename(infile);
-        const base = bname[0..bname.len - 3];
+        const base = infile[0..infile.len - 3];
         try files.append(allocator, base);
 
         debug(.DBG_BASIC, "Compiling Rulang source file: {s}\n", .{infile});
@@ -329,29 +319,28 @@ pub fn main() !void {
           _ = files.pop();
         }
       } else if (std.mem.eql(u8, ext, ".s") or std.mem.eql(u8, ext, ".asm") or std.mem.eql(u8, ext, ".as") or std.mem.eql(u8, ext, ".ars")) {
-      // When the option to compile-only is on
-      // It shall mean that all files are to be assembly
-      // Meaning do not handle assembly files
-      if (cfg.compileOnly) {
-        debug(.DBG_BASIC, "Compile-only option is set; skipping assembly file: {s}\n", .{infile});
-        _ = files.pop();
-        idx += 1;
-        continue;
-      }
+        // When the option to compile-only is on
+        // It shall mean that all files are to be assembly
+        // Meaning do not handle assembly files
+        if (cfg.compileOnly) {
+          debug(.DBG_BASIC, "Compile-only option is set; skipping assembly file: {s}\n", .{infile});
+          _ = files.pop();
+          idx += 1;
+          continue;
+        }
 
-      // Remove .s extension
-      const bname = std.fs.path.basename(infile);
-      const base = bname[0..bname.len - 2];
-      try files.append(allocator, base);
+        // Remove .s extension
+        const base = infile[0..infile.len - 2];
+        try files.append(allocator, base);
 
-      const status = callAssembler(base) catch {
-        _ = files.pop();
-        idx += 1;
-        continue;
-      };
-      if (status.Exited != 0) {
-        _ = files.pop();
-      }
+        const status = callAssembler(base) catch {
+          _ = files.pop();
+          idx += 1;
+          continue;
+        };
+        if (status.Exited != 0) {
+          _ = files.pop();
+        }
       } else {
         try stdout.print("Unsupported file extension: {s}\n", .{infile});
         try stdout.flush();
